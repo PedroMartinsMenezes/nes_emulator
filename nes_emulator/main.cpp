677997f2ecf0c5935a61acf25430e9d55010e023
pmmenezes <protopedro@gmail.com>
2026-01-19 00:29:31 -0300

    nestest

--- nes_emulator/main.cpp
+++ nes_emulator/main.cpp
@@ -2,7 +2,20 @@
 #include <cstdint>
 
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+    std::cout << "+--------------+\n";
+    std::cout << "| NES Emulator |\n";
+    std::cout << "+--------------+\n";
+
+    if (argc < 2)
+    {
+        std::cout << "[error] Missing ROM file\n";
+        return 1;
+    }
+
+    std::cout << "File: " << argv[1] << "\n";
+    std::cout << "Checking the emulator with 'nestest.nes' ROM\n";
+
+    return 0;
 }
