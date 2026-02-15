7579ebfe99ca232baad573b2cc0dd21a7eb153c2
pmmenezes <protopedro@gmail.com>
2026-02-15 19:16:06 -0300

    APU2A03

--- nes_emulator/apu2a03.cpp
+++ nes_emulator/apu2a03.cpp
@@ -1,23 +1,28 @@
 #include "apu2a03.h"
 
 
-APU2A03::APU2A03() {
+APU2A03::APU2A03()
+{
     //TODO incomplete
 }
 
-uint8_t APU2A03::cpuRead(uint16_t addr, uint8_t data) {
+uint8_t APU2A03::cpuRead(uint16_t addr, uint8_t data)
+{
     //TODO incomplete
     return 0;
 }
 
-void APU2A03::cpuWrite(uint16_t addr, uint8_t data) {
+void APU2A03::cpuWrite(uint16_t addr, uint8_t data)
+{
     //TODO incomplete
 }
 
-void APU2A03::reset() {
+void APU2A03::reset()
+{
     //TODO incomplete
 }
 
-void APU2A03::clock() {
+void APU2A03::clock()
+{
     //TODO incomplete
 }
