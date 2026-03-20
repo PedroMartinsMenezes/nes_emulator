1ac29d64179cad6d85ad58b2639ac54f1c4aa7b8
pmmenezes <protopedro@gmail.com>
2026-03-19 21:59:08 -0300

    Only main

--- nes_emulator/main.cpp
+++ nes_emulator/main.cpp
@@ -3,9 +3,6 @@
 #include <iostream>
 #include <memory>
 
-#include "cartridge.h"
-#include "nes.h"
-
 namespace fs = std::filesystem;
 
 int main(int argc, char** argv)
@@ -33,44 +30,6 @@ int main(int argc, char** argv)
     std::string log_name = fs::path(rom_path).stem().string() + ".log";
 
     std::cout << "Loading: " << file_name << "\n";
-
-    NES nes;
-
-    auto cart = std::make_shared<Cartridge>(rom_path);
-
-    if (!cart->isValid())
-    {
-        std::cout << "Invalid or unsupported ROM.\n";
-        return 1;
-    }
-
-    nes.insertCartridge(cart);
-    nes.reset();
-
-    std::cout << "Starting execution...\n";
-
-    // specific settings for 'nestest.nes'
-    if (file_name == "nestest.nes")
-    {
-        nes.bus.cpu->PC = 0xC000;
-        nes.last_pc = 0xC66E;
-    }
-    else if (file_name == "ppu_vbl_nmi.nes")
-    {
-        nes.detailed_log = true;
-    }
-
-    std::ofstream log(log_name);
-
-    nes.cpu.setLogger(&log);
-
-    // Nintendulator-style execution. CPU drives timing internally.
-    while (true)
-    {
-        nes.cpu.ExecOp();
-    }
-
-    log.close();
 
     return 0;
 }
